89821478d4aa1ea4b909891b8275cec5242806df
Kim Cuc Ngo <kimcucngo@Kims-MacBook-Air.local>
2022-12-24 23:53:46 +0900

    Update a post

--- Blog-Website/resources/views/admin/posts/index.blade.php
+++ Blog-Website/resources/views/admin/posts/index.blade.php
@@ -1,6 +1,13 @@
 <x-admin-master>
     @section('content')
     <h1>All posts</h1>
+    @if(session('message'))
+    <div class="alert alert-danger">{{session('message')}}</div>
+    @elseif(session('post-created-message'))
+    <div class="alert alert-success">{{session('post-created-message')}}</div>
+    @elseif(session('post-updated-message'))
+    <div class="alert alert-success">{{session('post-updated-message')}}</div>
+    @endif
     <div class="card shadow mb-4">
         <div class="card-header py-3">
           <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
@@ -16,7 +23,7 @@
                   <th>Image</th>
                   <th>Created At</th>
                   <th>Updated At</th>
-                  <th>Delete</th>>
+                  <th>Delete</th>
                 </tr>
               </thead>
               <tfoot>
@@ -35,16 +42,16 @@
                     @foreach($posts as $post)
                         <td>{{$post->id}}</td>
                         <td>{{$post->user->name}}</td>
-                        <td>{{$post->title}}</td>
+                        <td><a href="{{route('post.edit',$post->id)}}">{{$post->title}}</a></td>
                         <td>
                             <img height="40px" src="{{$post->post_image}}" alt="">
                         </td>
                         <td>{{$post->created_at->diffForHumans()}}</td>
                         <td>{{$post->updated_at->diffForHumans()}}</td>
                         <td>
-                          <form method="post" action="{{route('post.destroy',$post->id)}}," entype="multipart/form-data">
+                          <form method="post" action="{{route('post.destroy',$post->id)}}" entype="multipart/form-data">
                             @csrf
-                            @method['DELETE']
+                            @method('DELETE')
                            <button type="submit" class="btn btn-danger">Delete</button> 
                           </form>
                         </td>
--- Blog-Website/routes/web.php
+++ Blog-Website/routes/web.php
@@ -8,9 +8,10 @@ Route::get('/post/{post}', [App\Http\Controllers\PostController::class, 'show'])
 
 Route::middleware('auth')->group(function(){
     Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
-    
     Route::get('/admin/posts', [App\Http\Controllers\PostController::class, 'index'])->name('post.index');
     Route::get('/admin/posts/create', [App\Http\Controllers\PostController::class, 'create'])->name('post.create');
     Route::post('/admin/posts', [App\Http\Controllers\PostController::class, 'store'])->name('post.store');
-    Route::delete('/admin/posts/{post}'/destroy, [App\Http\Controllers\PostController::class, 'destroy'])->name('post.destroy');
+    Route::get('/admin/posts/{post}/edit', [App\Http\Controllers\PostController::class, 'edit'])->name('post.edit');
+    Route::delete('/admin/posts/{post}/destroy', [App\Http\Controllers\PostController::class, 'destroy'])->name('post.destroy');
+    Route::patch('/admin/posts/{post}/update', [App\Http\Controllers\PostController::class, 'update'])->name('post.update');
 });
